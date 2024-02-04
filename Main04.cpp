a62fe637051ba9140a3c508379ad39ac7a9db63c
Tejus Setlur <tejus_setlur@tejuss-mbp.mynetworksettings.com>
2024-02-04 09:51:13 -0500

    Pricing an asian call option

---
+++ Main04.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "PathDepOption.h"
+#include "GmtrAsianCall.h"
+
+using namespace std;
+using namespace fre;
+
+int main()
+{
+    double S0=100.0, r=0.03, sigma=0.2;
+    MCModel Model(S0,r,sigma);
+
+    double T =1.0/12.0, K=100.0;
+    int m=30;
+
+    ArthmAsianCall Option(T,K,m);
+    GmtrAsianCall  CVOption(T,K,m);
+
+    long N=30000;
+    double epsilon =0.001;
+    Option.PriceByVarRedMC(Model,N,CVOption,epsilon);
+    cout << "Arithmetic call price = " << Option.GetPrice() << endl
+        << "Error = " << Option.GetPricingError() << endl 
+        << "delta = " << Option.GetDelta() << endl << endl;
+
+    Option.PriceByMC(Model,N,epsilon);
+    cout << "Price by direct MC = " << Option.GetPrice() << endl
+        << "Error = " << Option.GetPricingError() << endl
+        << "delta = " << Option.GetDelta() << endl << endl;
+
+    return 0;
+}
+/*
+Arithmetic call price = 1.42593
+Error = 0.000139083
+delta = 0.520085
+
+Price by direct MC = 1.40875
+Error = 0.01194
+delta = 0.52316
+
+*/
+
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pricing_asian_call
 Pricing an asian geometric call
+
+Input:
+
+   double S0=100.0, r=0.03, sigma=0.2;
+   double T =1.0/12.0, K=100.0;
+   int m=30;
+   long N=30000;
+   double epsilon =0.001;
+
+Output:
+
+Arithmetic call price = 1.42609
+Error = 0.000138491
+delta = 0.520246
+
+Price by direct MC = 1.41326
+Error = 0.0119483
+delta = 0.525525
